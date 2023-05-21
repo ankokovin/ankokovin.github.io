3b26c96bdc7bb25304a2a95aa1a3b753bccee95a
Aleksey Kokovin <rycarok@gmail.com>
2023-05-21 22:00:58 +0500

    accessibility fix: added title to <iframe>

--- src/components/project/DisplayProject.tsx
+++ src/components/project/DisplayProject.tsx
@@ -10,7 +10,7 @@ export default function DisplayProject() {
 	return <main>
 		<div className='display-project'>
 			<h1>{project["ru-name"]}</h1>
-			<iframe src={`https://ankokovin.github.io/${project.id}`}></iframe>
+			<iframe src={`https://ankokovin.github.io/${project.id}`} title={`Project "${project.id}"`}></iframe>
 		</div>
 	</main> ;
 }
