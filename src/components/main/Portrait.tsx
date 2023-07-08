64cafc9770796f96e7d1bee47b954cb64c56367f
Aleksey Kokovin <rycarok@gmail.com>
2023-07-08 18:38:35 +0500

    #18 added alt text to portrait

--- src/components/main/Portrait.tsx
+++ src/components/main/Portrait.tsx
@@ -7,6 +7,6 @@ import myLogoRaw from "/icon.svg?raw";
 export default function Portait() {
 	return <div className="portrait">
 		<div className="portrait-logo" dangerouslySetInnerHTML={{__html: myLogoRaw}}></div>
-		<img className="portrait-image" src={myPortrait}/>
+		<img className="portrait-image" src={myPortrait} alt="Traced selfie in almost minimalist style"/>
 	</div>;
 }
