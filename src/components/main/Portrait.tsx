b7ebf66d4340fbd69402ebf6755a5200911ec2e5
Aleksey Kokovin <rycarok@gmail.com>
2023-07-18 14:04:20 +0500

    partially remade in anime style

--- src/components/main/Portrait.tsx
+++ src/components/main/Portrait.tsx
@@ -1,12 +1,12 @@
 import "./Portrait.css";
 
-import myPortrait from "Assets/self-portrait.svg";
+import myPortrait from "Assets/self-portrait-anime.min.svg";
 
 import myLogoRaw from "/icon.svg?raw";
 
 export default function Portait() {
 	return <div className="portrait">
 		<div className="portrait-logo" dangerouslySetInnerHTML={{__html: myLogoRaw}}></div>
-		<img className="portrait-image" src={myPortrait} alt="Traced selfie in almost minimalist style"/>
+		<img className="portrait-image" src={myPortrait} alt="Traced selfie partially redrawn in anime style"/>
 	</div>;
 }
