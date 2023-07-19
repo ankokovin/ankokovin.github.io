94fb51be347507d8190e0e4da78bc7cb450a0436
Aleksey Kokovin <rycarok@gmail.com>
2023-07-19 18:43:55 +0500

    fix layout for projects view

--- src/components/project/DisplayProject.tsx
+++ src/components/project/DisplayProject.tsx
@@ -7,8 +7,8 @@ import { Project } from "../../types";
 export default function DisplayProject() {
 	const project = useLoaderData() as Project;
 
-	return <main>
-		<div className='display-project'>
+	return <main className="display-project">
+		<div>
 			<h1>{project["ru-name"]}</h1>
 			<iframe src={`https://ankokovin.github.io/${project.id}`} title={`Project "${project.id}"`}></iframe>
 		</div>
